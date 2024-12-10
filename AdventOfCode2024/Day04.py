c1ff1d23666a15ba86c3452c35cc53f44b48daae
Shaun Levy <SLY@ndonline.ac.uk>
2024-12-10 12:04:02 +0000

    completing day 4

--- AdventOfCode2024/Day04.py
+++ AdventOfCode2024/Day04.py
@@ -1,3 +1,42 @@
 data = open("Inputs/Day04.txt").read().split("\n")
 
-print(data)
+def part01():
+    lines = []
+    for i in range(len(data)):
+        for j in range(len(data)):
+            if i + 4 <= len(data):
+                line = "".join([data[i + n][j] for n in range(4)])
+                lines.append(line)
+                lines.append(line[::-1])
+            if j + 4 <= len(data[i]):
+                line = data[i][j:j+4]
+                lines.append(line)
+                lines.append(line[::-1])
+            if i + 4 <= len(data) and j + 4 <= len(data[i]):
+                line = "".join([data[i+n][j+n] for n in range(4)])
+                lines.append(line)
+                lines.append(line[::-1])
+            if i + 4 <= len(data) and j - 4 >= -1:
+                line = "".join([data[i+n][j-n] for n in range(4)])
+                lines.append(line)
+                lines.append(line[::-1])
+    print(lines.count("XMAS"))
+
+def check_cross(block):
+    print(block)
+    #two cross shapes to check:
+    #   .?.     ?.?
+    #   ?A?     .A.
+    #   .?.     ?.?
+
+for i in range(len(data)):
+    for j in range(len(data)):
+        if data[i][j] == "A":
+            print("checking...")
+            if i - 1 >= 0 and j - 1 >= 0 and i + 1 < len(data) and j + 1 < len(data):
+                block = []
+                for x in [-1,0,1]:
+                    print(data[i+x][j-1:j+2])
+                    block.append(list(data[i+x][j-1:j+2]))
+                check_cross(block)
+            print()
